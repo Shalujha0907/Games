aaf8a568ca69280ddd403714d70914b5ba9d9824
Shalu Jha <sjha18788@gmail.com>
2024-11-24 21:39:45 +0530

    Update rock_paper_scissor.js

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -5,9 +5,44 @@ const p2Name = prompt("Enter player2's name = ");
 let p1Score = 0;
 let p2Score = 0;
 
-console.log("Enter '1' for stone 🪨\n");
-console.log("Enter '2' for paper 📜\n");
+console.log("Enter '1' for stone ✊\n");
+console.log("Enter '2' for paper 🖐️\n");
 console.log("Enter '3' for scissor ✂️\n");
+
+const rock = "┏━━━━━━━━┓ \n┃   ✊   ┃ \n┗━━━━━━━━┛";
+const paper = "┏━━━━━━━━┓ \n┃   🖐️    ┃ \n┗━━━━━━━━┛";
+const scissor = "┏━━━━━━━━┓ \n┃   ✌️    ┃ \n┗━━━━━━━━┛";
+
+function timeDelay() {
+  for (let count = 0; count < 9; count++) {
+    for (let timeDelay = 0; timeDelay < count * 10000000; timeDelay++) {
+    }
+  }
+}
+
+function animation(input) {
+  console.clear();
+  timeDelay();
+  console.log(paper);
+  timeDelay();
+  console.clear();
+  console.log(scissor);
+  timeDelay();
+  console.clear();
+  console.log(rock);
+  timeDelay();
+  console.clear();
+  console.log(scissor);
+  timeDelay();
+  console.clear();
+  console.log(paper);
+  timeDelay();
+  console.clear();
+  console.log(rock);
+  timeDelay();
+  console.clear();
+  return input;
+}
 
 function getInput(combo) {
   switch (combo) {
@@ -31,20 +66,20 @@ function getInput(combo) {
 function getEmoji(input) {
   switch (input) {
     case 1:
-      return "🪨";
+      return rock;
     case 2:
-      return "📜";
+      return paper;
     case 3:
-      return "✂️";
+      return scissor;
   }
 }
 
 function selectWinner(p1Score, p2Socre) {
   if (p1Score === p2Socre) {
-    return " The Match is Draw 🫤🫤";
+    return "OOOPSS!!! The Match is Draw 🫤🫤";
   }
 
-  return p1Score > p2Socre ? p1Name + " WONN🥳😎" : p2Name + " WON🥳😎";
+  return p1Score > p2Socre ? p1Name + " WONN🥳😎 \n" : p2Name + " WON🥳😎\n";
 }
 
 function selectWinningRound(combo) {
@@ -59,20 +94,19 @@ function selectWinningRound(combo) {
   }
 
   if (getInput(combo) === "draw") {
-    console.log("this round is Draw 🫤");
+    console.log("this round is Draw 🫤 \n");
   }
 }
 
 function startGame() {
   for (let matchCount = 0; matchCount < 3; matchCount++) {
     prompt(p1Name + " Shake your Hand with " + p2Name + " and press enter");
-    prompt(p2Name + " Shake your Hand with " + p1Name + " and press enter");
-
     const input1 = Math.ceil(Math.random() * 3);
-    const input2 = Math.ceil(Math.random() * 3);
+    console.log(p1Name + " you got this:\n" + animation(getEmoji(input1) + "\n"));
 
-    console.log(p1Name + " you got this: " + getEmoji(input1) + "\n");
-    console.log(p2Name + " you got this: " + getEmoji(input2) + "\n");
+    prompt(p2Name + " Shake your Hand with " + p1Name + " and press enter");
+    const input2 = Math.ceil(Math.random() * 3);
+    console.log(p2Name + " you got this:\n" + animation(getEmoji(input2)) + "\n");
 
     const combo = '' + input1 + input2;
     selectWinningRound(combo);
